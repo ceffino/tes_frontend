dc0bd4ca125f0d5ad7eb597cd8eca9a4605796d2
ceffino <finomenal0@gmail.com>
2022-12-11 11:00:03 +0700

    update

--- Program/script.js
+++ Program/script.js
@@ -1,3 +1,20 @@
+// Cek bilangan
+let range = 15;
+let result = []
+
+for (let i = 1; i < range; i++){
+    if (i % 7 == 0) {
+        result.push('bestada')
+        continue
+    }
+    result.push(i)
+}
+result.push('sukses')
+result =result.join(',')
+console.log(result);
+
+
+
 //Cek nilai array yg sama
 const angka = [3, 7, 1, 2, 6, 7, 8, 8, 12, 5, 3, 12];
 
